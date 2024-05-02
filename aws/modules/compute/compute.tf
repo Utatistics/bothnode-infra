92648166c51006cd7067cee5fcc1000710c921f8
Utashi Fujimoto <frusciante2580@gmail.com>
2024-05-02 16:01:49 +0000

    add: attaching sg to ec2

--- aws/modules/compute/compute.tf
+++ aws/modules/compute/compute.tf
@@ -1,19 +1,20 @@
+module "sg" {
+    source = "../network" 
+}
 
 resource "aws_instance" "ec2_bothnode" {
-  ami           = "ami-0f7b55661ecbbe44c"
-  instance_type = "m5.xlarge"
-  key_name      = "ec2key"
+    ami                    = "ami-0f7b55661ecbbe44c"
+    instance_type          = "m5.xlarge"
+    key_name               = "ec2key"
 
-  subnet_id = "subnet-0cc3a5666f182e7e1"
-
-  vpc_security_group_ids = [aws_security_group.sg_bothnode.id]
-
-  tags = {
-    Name = "EC2BothNode"
-  }
-
-  root_block_device {
-    volume_size = 2000
+    subnet_id              = "subnet-0cc3a5666f182e7e1"
+    vpc_security_group_ids = [module.sg.aws_security_group.sg_bothnode.id]
+    
+    tags                   = {
+        Name               = "EC2BothNode"
+    }
+    root_block_device {
+        volume_size        = 2000
   }
 }
 
